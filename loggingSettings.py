118ca53e699675e856c516daeb6d0f77a6dd85a7
Jakub Popławski <32838101+jakubpoplawski@users.noreply.github.com>
2024-06-06 16:08:53 +0200

    Update loggingSettings.py
    Added missing comments to the logging functions.

--- loggingSettings.py
+++ loggingSettings.py
@@ -3,6 +3,12 @@ import pathlib
 from portability import resource_path
 
 def logger_initialization(log_name):
+    """The function initialises a logger instance. And sets a logger 
+    file.
+
+    Args:
+        log_name (str): Name of the logging file.
+    """
     global logger
 
     logger = logging.getLogger(__name__)
@@ -15,10 +21,17 @@ def logger_initialization(log_name):
         resource_path(f'Settings/{log_name}')))
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
+    
     return logger
 
 
 def logger_wrapper(func):
+    """The wrapper function handles logging information for
+    fired functions.
+
+    Args:
+        func (function): Wrapped function.
+    """
     def log_wrapper(*args):
         logger.info(f'{func.__name__} Section started.')
         try:
@@ -26,5 +39,7 @@ def logger_wrapper(func):
         except Exception as e:
             logger.error(e)
         logger.info('Section ended.')
+        
         return result
+        
     return log_wrapper
